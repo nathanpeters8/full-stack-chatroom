b61ac31e184333cf7132fe6c88c1e472c19fd9a6
h8rry <git@harrychen.net>
2022-12-06 17:48:00 +0800

    Remove users and simplify tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_18_113612) do
+ActiveRecord::Schema.define(version: 2022_03_18_113042) do
 
   create_table "tasks", force: :cascade do |t|
     t.integer "user_id"
@@ -20,11 +20,6 @@ ActiveRecord::Schema.define(version: 2022_03_18_113612) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_tasks_on_user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
