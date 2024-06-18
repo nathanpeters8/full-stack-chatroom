fba0ba100330b57da5538c26499d4e57060eb317
Nathan Peters <peters.nate8@gmail.com>
2024-06-17 22:19:28 -0600

    can send messages from server to browser now and update chat with most recent messages

--- app/javascript/channels/chat_channel.js
+++ app/javascript/channels/chat_channel.js
@@ -1,4 +1,6 @@
 import consumer from "./consumer"
+import $ from 'jquery';
+import { indexChats } from '../lib/requests.js'
 
 consumer.subscriptions.create("ChatChannel", {
   connected() {
@@ -8,9 +10,30 @@ consumer.subscriptions.create("ChatChannel", {
 
   disconnected() {
     // Called when the subscription has been terminated by the server
+    console.log('Front-end: disconnected from the chat channel!');
   },
 
   received(data) {
     // Called when there's incoming data on the websocket for this channel
+    console.log('Front-end: received data from the chat channel: ', data);
+
+    indexChats(function (response) {
+      var htmlString = response.chats.map(function(chat) {
+        return `
+          <li class="py-4">
+            <div class="flex space-x-3">
+              <div class="flex-1 space-y-1">
+                <div class="flex items-center justify-between">
+                  <h3 class="text-sm font-medium">${chat.name}</h3>
+                  <p class="text-sm text-gray-500">${chat.ago}</p>
+                </div>
+                <p class="text-sm text-gray-500">${chat.message}</p>
+              </div>
+            </div>
+          </li>
+        `;
+      });
+      $('#chats').html(htmlString);
+    })
   }
 });
--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,3 +1,11 @@
 class Chat < ApplicationRecord
   validates :message, length: { maximum: 280 }, presence: true
+
+  after_create :stream_new_chat
+
+  private
+
+  def stream_new_chat
+    ActionCable.server.broadcast('chat_homepage', {body: self.message})
+  end
 end
